d8976a2429e0bf7612c5a24df040e3104cf6f7de
Sajal <nospamsajal@gmail.com>
2024-01-07 17:03:31 +0545

    Login based on user type

--- EscrowMAUI/ViewModel/LoginViewModel.cs
+++ EscrowMAUI/ViewModel/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace EscrowMAUI.ViewModel;
 
@@ -7,5 +8,25 @@ public partial class LoginViewModel : ObservableObject
     public LoginViewModel()
     {
 
+    }
+    [ObservableProperty]
+    string email;
+
+    [ObservableProperty]
+    string password;
+
+    [ObservableProperty]
+    bool _isFormValid = false;
+
+    [ObservableProperty]
+    string _selectedUserType = "consumer";
+
+    [ObservableProperty]
+    string emailRegex = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+
+    [RelayCommand]
+    async Task LoginRequested()
+    {
+        await Task.CompletedTask;
     }
 }
--- EscrowMAUI/Views/LoginPage.xaml.cs
+++ EscrowMAUI/Views/LoginPage.xaml.cs
@@ -4,10 +4,13 @@ namespace EscrowMAUI.Views;
 
 public partial class LoginPage : ContentPage
 {
+    private readonly LoginViewModel _loginViewModel;
+
     public LoginPage(LoginViewModel loginViewModel)
     {
         InitializeComponent();
         BindingContext = loginViewModel;
+        _loginViewModel = loginViewModel;
     }
 
     protected override void OnAppearing()
@@ -15,5 +18,35 @@ public partial class LoginPage : ContentPage
         base.OnAppearing();
         EmailEntry.Text = "";
         PasswordEntry.Text = "";
+    }
+
+    private void UserTypeButton_Clicked(object sender, EventArgs e)
+    {
+        var btn = sender as Button;
+        btn.BackgroundColor = Colors.Blue;
+        string name = btn.StyleId;
+
+        if (name == "ProviderButton")
+        {
+            ConsumerButton.BackgroundColor = Colors.AntiqueWhite;
+            _loginViewModel.SelectedUserType = "provider";
+        }
+        else
+        {
+            ProviderButton.BackgroundColor = Colors.AntiqueWhite;
+            _loginViewModel.SelectedUserType = "consumer";
+        }
+    }
+
+    private void Form_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (EmailValidationBehavior.IsValid)
+        {
+            _loginViewModel.IsFormValid = true;
+        }
+        else
+        {
+            _loginViewModel.IsFormValid = false;
+        }
     }
 }
